3fd6241bfb495ddaee732613b2fab2d5bee4d5eb
BlablaTara <tara0561@stud.kea.dk>
2025-06-11 14:42:06 +0200

    Added more F/V and healt benefits

--- server/utils/resetDB.js
+++
@@ -1,7 +0,0 @@
-import pool from '.db.js';
-
-await pool.query(`DROP TABLE IF EXISTS user_fruit_selections, user_vegetable_selections, fruits, vegetables, users CASCADE;`);
-console.log("Dropped all tables");
-
-import './setupDB.js';
-import './seedFruitsNVeggies.js';
